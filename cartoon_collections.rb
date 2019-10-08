678a6bdd9baf2b292e53bd86ecd43641213db546
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-08 19:48:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,9 @@ def long_planeteer_calls(calls)
   end 
 end
 
-def find_the_cheese(cheeses)
-   cheese_types = ["cheddar", "gouda", "camembert"]
-   
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
